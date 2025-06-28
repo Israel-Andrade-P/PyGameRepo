2afe73abf91fdfb71199ce78e3ecfbcaa07049ec
Zel <israel88andrade@gmail.com>
2025-06-28 10:52:53 -0300

    collision tests

--- Vampire_Survivor/code/player.py
+++ Vampire_Survivor/code/player.py
@@ -1,12 +1,15 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.image.load(join("images", "player", "down", "0.png")).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
+
+        #movement
         self.direction = pygame.math.Vector2()
         self.speed = 300
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -16,7 +19,20 @@ class Player(pygame.sprite.Sprite):
         
 
     def move(self, dt):
-        self.rect.center += self.direction * self.speed * dt   
+        self.rect.x += self.direction.x * self.speed * dt  
+        self.collision("horizontal")
+        self.rect.y += self.direction.y * self.speed * dt 
+        self.collision("vertical")
+
+    def collision(self, direction):
+        for col_sprite in self.collision_sprites:
+            if col_sprite.rect.colliderect(self.rect):
+                if direction == "horizontal":
+                    if self.direction.x > 0: self.rect.right = col_sprite.rect.left
+                    if self.direction.x < 0: self.rect.left = col_sprite.rect.right
+                else:
+                    if self.direction.y > 0: self.rect.bottom = col_sprite.rect.top
+                    if self.direction.y < 0: self.rect.top = col_sprite.rect.bottom    
 
     def update(self, dt):
         self.input()
