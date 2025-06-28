ed805b3dc4378d8d1a8b3a043433d761f81900f2
Zel <israel88andrade@gmail.com>
2025-06-28 11:53:21 -0300

    loading map

--- Vampire_Survivor/code/player.py
+++ Vampire_Survivor/code/player.py
@@ -5,6 +5,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load(join("images", "player", "down", "0.png")).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
+        self.hitbox_rect = self.rect.inflate(-60, 0)
 
         #movement
         self.direction = pygame.math.Vector2()
@@ -19,20 +20,21 @@ class Player(pygame.sprite.Sprite):
         
 
     def move(self, dt):
-        self.rect.x += self.direction.x * self.speed * dt  
+        self.hitbox_rect.x += self.direction.x * self.speed * dt  
         self.collision("horizontal")
-        self.rect.y += self.direction.y * self.speed * dt 
+        self.hitbox_rect.y += self.direction.y * self.speed * dt 
         self.collision("vertical")
+        self.rect.center = self.hitbox_rect.center
 
     def collision(self, direction):
         for col_sprite in self.collision_sprites:
-            if col_sprite.rect.colliderect(self.rect):
+            if col_sprite.rect.colliderect(self.hitbox_rect):
                 if direction == "horizontal":
-                    if self.direction.x > 0: self.rect.right = col_sprite.rect.left
-                    if self.direction.x < 0: self.rect.left = col_sprite.rect.right
+                    if self.direction.x > 0: self.hitbox_rect.right = col_sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = col_sprite.rect.right
                 else:
-                    if self.direction.y > 0: self.rect.bottom = col_sprite.rect.top
-                    if self.direction.y < 0: self.rect.top = col_sprite.rect.bottom    
+                    if self.direction.y > 0: self.hitbox_rect.bottom = col_sprite.rect.top
+                    if self.direction.y < 0: self.hitbox_rect.top = col_sprite.rect.bottom    
 
     def update(self, dt):
         self.input()
